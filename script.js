1bc46f10e98bde17be32b807389a36ea4be4115a
LeedsByExample <leeda.minkin@yahoo.com>
2023-11-12 22:38:51 -0600

    Completed Task 2, added validation

--- script.js
+++ script.js
@@ -2,6 +2,19 @@
 
 window.addEventListener("load", function() {
 
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        let pilot = document.querySelector("input[name=pilotName]").value;
+        let copilot = document.querySelector("input[name=copilotName]").value;
+        let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
+        let cargoLevel = document.querySelector("input[name=cargoMass]").value;
+        let list = document.getElementById("faultyItems");
+
+        formSubmission(document,list,pilot,copilot,fuelLevel,cargoLevel);
+    });
+
+    
     let listedPlanets;
     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
     let listedPlanetsResponse;
--- scriptHelper.js
+++ scriptHelper.js
@@ -18,12 +18,78 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
  }
  
  function validateInput(testInput) {
-    
+    if (testInput === "") {
+        return "Empty";
+    } else if (isNaN(testInput)) {
+        return "Not a Number";
+    } else {
+        return "Is a Number";
+    };
  }
- 
+
  function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-    
- }
+    let pilotStatus = document.getElementById("pilotStatus");
+    let copilotStatus = document.getElementById("copilotStatus");
+    let fuelStatus = document.getElementById("fuelStatus");
+    let cargoStatus = document.getElementById("cargoStatus");
+    let launchStatus = document.getElementById("launchStatus");
+
+    if (validateInput(pilot) === "Empty" || validateInput(copilot) === "Empty" || validateInput(fuelLevel) === "Empty" || validateInput(cargoLevel) === "Empty") {
+        window.alert("All fields required.");
+    } else { 
+
+        let readyForLaunch = true;
+
+        //pilot check
+        if (validateInput(pilot) === "Is a Number") {
+            window.alert("Pilot selection invalid.");
+            readyForLaunch = false;
+        } else {
+            pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
+        }
+        
+        //copilot check
+        if (validateInput(copilot) === "Is a Number") {
+            window.alert("Co-pilot selection invalid.");
+            readyForLaunch = false;
+        } else {
+            copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
+        }
+        
+        //fuel check
+        if (validateInput(fuelLevel) === "Not a Number") {
+            window.alert("Fuel level must be a number.");
+            readyForLaunch = false;
+        } else if (Number(fuelLevel) < 10000) {
+            list.style.visibility = "visible";
+            fuelStatus.innerHTML = "Fuel level too low for launch";
+            readyForLaunch = false;
+        } else {
+            fuelStatus.innerHTML = "Fuel level high enough for launch";
+        }
+
+        //cargo check
+        if (validateInput(cargoLevel) === "Not a Number") {
+            window.alert("Cargo mass must be a numerical value.");
+            readyForLaunch = false;
+        } else if (Number(cargoLevel) > 10000) {
+            list.style.visibility = "visible";
+            cargoStatus.innerHTML = "Cargo mass too heavy for launch";
+            readyForLaunch = false;
+        } else {
+            cargoStatus.innerHTML = "Cargo mass low enough for launch";
+        }
+
+        //final launch check
+        if (readyForLaunch === true) {
+            launchStatus.style.color = "green";
+            launchStatus.innerHTML = "Shuttle is Ready for Launch";
+        } else {
+            launchStatus.innerHTML = "Shuttle Not Ready for Launch";
+            launchStatus.style.color = "red";
+        }
+    } 
+}
  
  async function myFetch() {
      let planetsReturned;
@@ -42,3 +108,8 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
  module.exports.formSubmission = formSubmission;
  module.exports.pickPlanet = pickPlanet; 
  module.exports.myFetch = myFetch;
+
+
+
+
+
